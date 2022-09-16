d1e873d2efeff2294e29777a815c80ab933fa16b
OrlovVasily <orlov.v@amayasoft.uz>
2022-09-16 21:31:02 +0300

    Add more test. Change naming

--- OrlovAnalyzer/SimpleYoYoAnalyzer.cs
+++ OrlovAnalyzer/SimpleYoYoAnalyzer.cs
@@ -1,35 +1,37 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Collections.Immutable;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-using System;
-using System.Collections.Immutable;
-using System.Linq;
 
-namespace AsyncAnalyzerReady
+namespace OrlovAnalyzer
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class SimpleYoYoAnalyzer : DiagnosticAnalyzer
     {
-        string id = "TEST1337";
-        string title = "Test empty analzyer";
-        string description = "yo-yo problem";
-        string category = "EmptyTest";
-        DiagnosticSeverity severity = DiagnosticSeverity.Error;
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Diagnostic);
+        private const string Id = "Yo-Yo problem";
+        private const string Title = "Simple Yo-Yo problem analyzer";
+        private const string Description = "Method declaration above invocation";
+        private const string Category = "Antipattern";
 
-        DiagnosticDescriptor m_diagnostic;
+        private const DiagnosticSeverity Severity = DiagnosticSeverity.Hidden;
 
-        protected DiagnosticDescriptor diagnostic =>
-            m_diagnostic = m_diagnostic ?? new DiagnosticDescriptor(id, title, description, category, severity, true);
+        private DiagnosticDescriptor Diagnostic =>
+            _mDiagnostic = _mDiagnostic ?? new DiagnosticDescriptor(Id, Title, Description, Category, Severity, true);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(diagnostic);
+        private DiagnosticDescriptor _mDiagnostic;
 
 
         public override void Initialize(AnalysisContext context)
         {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze |
+                                                   GeneratedCodeAnalysisFlags.ReportDiagnostics);
             context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.InvocationExpression);
         }
-
 
         private void Analyze(SyntaxNodeAnalysisContext ctx)
         {
@@ -49,21 +51,23 @@ namespace AsyncAnalyzerReady
                 case MethodDeclarationSyntax methodDeclarationSyntax:
                     method = methodDeclarationSyntax;
                     break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+            }
+
+            if (method is null)
+            {
+                return;
             }
 
             var allClassMethods = ((ClassDeclarationSyntax)method.Parent).Members.OfType<MethodDeclarationSyntax>();
 
             var correctMethod = allClassMethods.FirstOrDefault(x =>
-                x.Identifier.Text == (invocationMethod.Expression is IdentifierNameSyntax ins ? ins.Identifier.Text : ""));
-            
-            if(correctMethod != null && correctMethod.SpanStart < method.SpanStart)
-            {
-                ctx.ReportDiagnostic(Diagnostic.Create(diagnostic, ctx.Node.GetLocation()));
-            }
+                x.Identifier.Text ==
+                (invocationMethod.Expression is IdentifierNameSyntax ins ? ins.Identifier.Text : ""));
 
+            if (correctMethod != null && correctMethod.SpanStart < method.SpanStart)
+            {
+                ctx.ReportDiagnostic(Microsoft.CodeAnalysis.Diagnostic.Create(Diagnostic, ctx.Node.GetLocation()));
+            }
         }
     }
 }
-
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,105 +1,35 @@
 using System;
 using System.Collections.Immutable;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
-using AsyncAnalyzerReady;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OrlovAnalyzer;
 
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public async Task TestWithAnotherClass()
-        {
-            const string code = @"
-    class Program
-    {
-        AnotherClass anotherClass = new AnotherClass();
-        static void  Main(string[] args)
-        {
-        }
-        public void FirstMethod(){}
-        public void SimpleMethod()
-        {
-            FirstMethod();
-            DownMethod();
-            anotherClass.SimpleMethod();
-        }
-
-        public void DownMethod(){}
-
-    }
-
-    class AnotherClass
-    {
-        public void SimpleMethod(){}
-    }
-";
-        }
-        
-        [TestMethod]
-        public async Task TestWithNestedClass(){}
-        [TestMethod]
-        public async Task TestWithInheritedClass(){}
-        
-        
-        [TestMethod]
-        public async Task SimpleTest()
-        {
-            const string code = @"
-    class Program
-    {
-        static void  Main(string[] args)
-        {
-        }
-        public void FirstMethod()
-        {
-        }
-        public void SimpleMethod()
-        {
-            FirstMethod();
-            DownMethod();
-        }
-
-        public void DownMethod()
-        {
-        }
-    }";
-            ImmutableArray<Diagnostic> diagnostics = await GetDiagnostics(code);
-
-            Assert.AreEqual(1, diagnostics.Length);
-        }
-
         private static async Task<ImmutableArray<Diagnostic>> GetDiagnostics(string code)
         {
-            AdhocWorkspace workspace = new AdhocWorkspace();
+            var workspace = new AdhocWorkspace();
 
             var solution = workspace.CurrentSolution;
 
             var projectId = ProjectId.CreateNewId();
 
-            solution = solution
-                .AddProject(
-                    projectId,
-                    "MyTestProject",
-                    "MyTestProject",
-                    LanguageNames.CSharp);
+            solution = solution.AddProject(projectId,"MyTestProject","MyTestProject",LanguageNames.CSharp);
 
-            solution = solution
-                .AddDocument(DocumentId.CreateNewId(projectId),
-                "File.cs",
-                code);
+            solution = solution.AddDocument(DocumentId.CreateNewId(projectId),"File.cs",code);
 
             var project = solution.GetProject(projectId);
 
-            project = project.AddMetadataReference(
-                MetadataReference.CreateFromFile(
-                    typeof(object).Assembly.Location))
+            project = project.AddMetadataReference(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
                 .AddMetadataReferences(GetAllReferencesNeededForType(typeof(ImmutableArray)));
 
             var compilation = await project.GetCompilationAsync();
@@ -125,6 +55,187 @@ namespace TestProject1
                 .Append(type.Assembly)
                 .Select(x => x.Location)
                 .ToImmutableArray();
+        }
+
+        [TestMethod]
+        public async Task TestWithAnotherClass()
+        {
+            const string code = @"
+            class Program
+            {
+                AnotherClass anotherClass = new AnotherClass();
+                static void  Main(string[] args){}
+
+                public void FirstMethod(){}
+
+                public void SimpleMethod()
+                {
+                    FirstMethod();
+                    DownMethod();
+                    anotherClass.SimpleMethod();
+                }
+
+                public void DownMethod(){}
+            }
+
+            class AnotherClass
+            {
+                public void SimpleMethod(){}
+            }";
+
+            var diagnostics = await GetDiagnostics(code);
+
+            Assert.AreEqual(1, diagnostics.Length);
+        }
+
+        [TestMethod]
+        public async Task TestWithLocalFunction()
+        {
+            const string code = @"
+    class Program
+    {
+        static void Main(string[] args) {}
+        public void FirstMethod() { }
+
+        public void SimpleMethod()
+        {
+            void LocalFunc1(){}
+
+            FirstMethod();
+            DownMethod();
+
+            LocalFunc1();
+            LocalFunc2();
+
+            void LocalFunc2() { }
+        }
+
+        public void DownMethod() { }
+
+    }";
+
+            var diagnostics = await GetDiagnostics(code);
+
+            Assert.AreEqual(1, diagnostics.Length);
+        }
+
+        [TestMethod]
+        public async Task TestWithNestedClass()
+        {
+            const string code = @"
+    class NestedClass : Program
+    {
+        public void ThirdMethod()
+        {
+            FirstMethod();
+            DownMethod();
+        }
+
+    } 
+    class Program
+    {
+        static void Main(string[] args) {}
+        public void FirstMethod() { }
+
+        public void SimpleMethod()
+        {
+            FirstMethod();
+            DownMethod();
+        }
+
+        public void DownMethod() { }
+
+    }
+";
+            var diagnostics = await GetDiagnostics(code);
+
+            Assert.AreEqual(1, diagnostics.Length);
+        }
+
+        [TestMethod]
+        public async Task TestWithInheritedClass()
+        {
+            const string code = @"
+    class Program
+    {
+        private InheritedClass _inheritedClass = new InheritedClass();
+        class InheritedClass
+        {
+            public void ThirdMethod(){}
+        }
+        static void Main(string[] args) {}
+        public void FirstMethod() { }
+
+        public void SimpleMethod()
+        {
+            FirstMethod();
+            DownMethod();
+            _inheritedClass.ThirdMethod();
+        }
+
+        public void DownMethod() { }
+
+    }
+";
+            var diagnostics = await GetDiagnostics(code);
+
+            Assert.AreEqual(1, diagnostics.Length);
+        }
+
+        [TestMethod]
+        public async Task TestWithStaticClass()
+        {
+            const string code = @"
+    public static class StaticClass
+    {
+        public static void StaticMethod() { }
+        public static void StaticMethod2() { }
+    }
+    class Program
+    {
+        static void Main(string[] args) {}
+        public void FirstMethod() { }
+
+        public void SimpleMethod()
+        {
+            FirstMethod();
+            DownMethod();
+            StaticClass.StaticMethod();
+            StaticClass.StaticMethod2();
+        }
+
+        public void DownMethod() { }
+
+    }
+";
+            var diagnostics = await GetDiagnostics(code);
+
+            Assert.AreEqual(1, diagnostics.Length);
+        }
+
+
+        [TestMethod]
+        public async Task SimpleTest()
+        {
+            const string code = @"
+            class Program
+            {
+                static void Main(string[] args){}
+
+                public void FirstMethod(){}
+
+                public void SimpleMethod()
+                {
+                    FirstMethod();
+                    DownMethod();
+                }
+
+                public void DownMethod(){}
+            }
+            ";
+            var diagnostics = await GetDiagnostics(code);
+
+            Assert.AreEqual(1, diagnostics.Length);
         }
     }
 }
